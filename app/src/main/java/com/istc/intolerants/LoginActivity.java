1fd87b748834a06c7d37a636c4fc002ccc7e1cfe
Manuel <manolomurodriguez@gmail.com>
2016-11-09 19:27:05 +0100

    Modificacion de las variables del Login Modificacion del login para entrar a la interfaz principal

--- app/src/main/java/com/istc/intolerants/LoginActivity.java
+++ app/src/main/java/com/istc/intolerants/LoginActivity.java
@@ -1,16 +1,33 @@
 package com.istc.intolerants;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btn_Nombre;
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
 
-    }
+        btn_Nombre = (Button) findViewById(R.id.Login_btn_Entrar);
 
+        btn_Nombre.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+            }
+        });
+    }
 }
